8a028de88151b124ff2b40835b30c708f5ecbed9
Chance McQueen <jasoncmcqueen@gmail.com>
2019-11-13 14:07:20 -0600

    updated to master via pull

---
+++ checkHash.py
@@ -0,0 +1,8 @@
+import json
+import datetime
+from blockchain import *
+from transaction import *
+
+#this will take a blockchain of a system and check the hashes to make sure they're correct
+def checkHash(blockchain):
+    return None
