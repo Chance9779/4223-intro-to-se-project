97f6116f071edff2bed6c3b40340fd1e7946824d
Chance McQueen <jasoncmcqueen@gmail.com>
2019-11-13 14:29:57 -0600

    added comment blocks to main.py

--- main.py
+++ main.py
@@ -19,12 +19,11 @@ class DaemonThread(Thread):
 
         Thread.__init__(self)
 
- 
-
     # Daemon Thread run method
 
     def run(self):
-
+        #this is an inherent function.  It will be called
+        #when start() is called on the daemon thread.
         #listen for another machine
         while True:
             try:
@@ -69,22 +68,39 @@ while(True):
     print("'t'    to create a transaction")
     print("'s'    to search through transactions")
     print("'x'    to exit")
+    #  t is to create a transaction
+    #
+    # s to search
+    #
+    # x to exit
+    #
+    # ignore anything else
+    #
     userInput = input("")
     if(userInput == 'x'):
         exit()
+    #create a transaction and update the blockchain
+    #
+    #
+    #************************TODO****************************
+    # this must be updated or put into a function so that it can
+    # send updated blocks to the other machine's in the 
     elif(userInput == 't'):
         newTransaction = transaction(1) #our store number is 1
         newTransaction.setTransaction()
         newBlock = newTransaction.makeBlock()
         updateBlockchain(newBlock)
         continue
+    #*********************************************************
     elif(userInput == 's'):
+        #this is the search tree.  This will continue until the user backs out of it 
         while(True):
             print("Type 'd' to search by date, 's' to search by store ID, or 't' to search by transaction id. (type 0 to return to main screen)")
             check = input("")
             if(check == 'd'):
                 searchByDateInterface()
                 continue
+            #store id search tree
             elif(check == 's'):
                 print("please type the store ID you wish to search for: ")
                 storeId = input("")
@@ -96,9 +112,11 @@ while(True):
                 except:
                     print("Something went wrong.  Make sure you enter an integer value")
                     continue
+            #transaction id search tree
             elif(check == 't'):
                 print("please type the transaction ID you wish to search for: ")
                 transId = input("")
+                #try to search by transaction id
                 try:
                     transId = int(transId)
                     results = searchByTransactionId(transId)
@@ -109,10 +127,11 @@ while(True):
                     continue
             elif(check == "0"):
                 break
+            #wasn't a valid character. ignore it
             else:
                 print("that character was not accepted.")
                 continue
-    
+    #wasn't a valid character. ignore it
     else:
         print("that character was not accepted.")
         continue
